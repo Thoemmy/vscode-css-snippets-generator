529108161d3f693cadc7d089d0dbf07ada4af9de
Thoemmy <t.kopitsch@starshiporigin.com>
2024-04-23 22:19:13 +0200

    update: basic functionallity ensured, simple file generation included

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # snippets-generator
+
 Creates snippets for css classes which then can be used to envolve your intelllisense in VSCode
+
+To use the generator type following in your cli
+`node main.js <PathToFile> <FileName>`
+
+this will go through the directory which is set by first argument and will select only css or scss files.
+
+the filename will be your set filename as second argument with the additionally added `yourFileName.extension.code-snippets`
---
+++ main.js
@@ -0,0 +1,64 @@
+var fs = require("fs");
+var path = require("path");
+
+function readFiles(directory) {
+  var files = fs.readdirSync(directory);
+  return files.filter(function (file) {
+    return file.endsWith(".scss") || file.endsWith(".css");
+  });
+}
+
+function analyzeClasses(fileContent) {
+  var classPattern = /\.([a-zA-Z0-9_-]+)/g;
+  var classNames = [];
+  var match;
+  while ((match = classPattern.exec(fileContent)) !== null) {
+    classNames.push(match[1]);
+  }
+  return classNames;
+}
+
+function generateObject(classes) {
+  var generatedObject = {};
+  classes.forEach(function (className) {
+    var classObject = {
+      scope: "html",
+      prefix: className,
+      body: [className],
+      description: className,
+    };
+    generatedObject[className] = classObject;
+  });
+  return generatedObject;
+}
+
+function createCodeSnippetsFile(directory, filename, generatedObject) {
+  const filePath = `${directory}/${filename}.extension.code-snippets`;
+  const content = JSON.stringify(generatedObject, null, 2);
+
+  fs.writeFile(filePath, content, "utf8", (err) => {
+    if (err) {
+      console.error("Fehler beim Schreiben der Datei:", err);
+    } else {
+      console.log(`Die Datei ${filePath} wurde erfolgreich erstellt.`);
+    }
+  });
+}
+
+function main() {
+  const filename = process.argv[3];
+  const directory = process.argv[2];
+  var files = readFiles(directory);
+  var allClasses = [];
+  files.forEach(function (file) {
+    var filePath = path.join(process.cwd(), directory, file);
+    var fileContent = fs.readFileSync(filePath, "utf-8");
+    var classes = analyzeClasses(fileContent);
+    allClasses.push.apply(allClasses, classes);
+  });
+  var generatedObject = generateObject(allClasses);
+  //console.log(JSON.stringify(generatedObject, null, 2));
+  createCodeSnippetsFile(directory, filename, generatedObject);
+}
+
+main();
